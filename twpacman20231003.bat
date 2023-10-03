aa67759d52d25c8a57f5f70a7c2454a9ef432370
IssyisBusy <122068899+IssyisBusy@users.noreply.github.com>
2023-10-03 21:23:41 +1000

    Update twpacman20231003.bat

--- twpacman20231003.bat
+++ twpacman20231003.bat
@@ -2,7 +2,7 @@
 Echo Install Adobe & VLC 
 REM Adobe  
 winget install Adobe.Acrobat.Reader.64-bit  
-if %ERRORLEVEL% EQU 0 Echo Powertoys installed successfully.  
+if %ERRORLEVEL% EQU 0 Echo Adobe Read DC Installed.  
 REM VLC  
 winget install VideoLAN.VLC  
-if %ERRORLEVEL% EQU 0 Echo Terminal installed successfully.   %ERRORLEVEL%
+if %ERRORLEVEL% EQU 0 Echo VLC Installed.   %ERRORLEVEL%
